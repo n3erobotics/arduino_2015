1405c658734130ee4eae8f5e4a7538b34718a490
n3eist <geraln3e@gmail.com>
2015-04-09 09:22:33 +0100

    Arduino libraries used in 2015

---
+++ Car/examples/car_example_serial/car_example_serial/car_example_serial.ino
@@ -0,0 +1,51 @@
+#include <Servo.h>
+#include "Car.h"
+#include "Rgb.h"
+Car car;
+char device;
+int value;
+
+void setup(){
+  Serial.begin(115200);
+  //Serial.println("Connected!");
+  car.attachMotor(6);
+  car.attachServo(9);
+  car.stop(); // in case of failure (safety)
+  delay(200);
+  car.neutral(); // in case of failure (safety)
+}
+
+void loop(){
+  if(Serial.available()){
+    delay(1);
+    device=Serial.read();
+    Serial.print(device);
+    switch(device){
+      case 'f':
+        car.setSpeed(FORWARD, Serial.parseInt());
+        break;
+      case 'b':
+        car.setSpeed(BACKWARD, Serial.parseInt());
+        break;
+      case 'r':
+        car.turn(RIGHT, Serial.parseInt());
+        break;
+      case 'l':
+        car.turn(LEFT, Serial.parseInt());
+        break;
+      case 's':
+        car.stop();
+        break;
+      case 'n':
+        car.neutral();
+        break;
+      default:
+        //DO NOTHING
+        break;
+    }
+  }
+  if(sensor_linha){
+    ler_sensor_linha();
+    car.stop();
+  }
+}
